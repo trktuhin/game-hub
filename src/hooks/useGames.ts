0c025fbd041d36384576a457bdfc1a7e03498972
trktuhin <robin35-908@diu.edu.bd>
2023-06-22 11:28:56 +0600

    Part 2 v33

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import apiClient, { FetchDataResponse } from "../services/api-client";
+import APIClient, { FetchDataResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
+
+const apiClient = new APIClient<Game>("/games");
 
 export interface Game {
   id: number;
@@ -15,16 +17,14 @@ const useGames = (gameQuery: GameQuery) =>
   useQuery<FetchDataResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: () =>
-      apiClient
-        .get<FetchDataResponse<Game>>("/games", {
-          params: {
-            genres: gameQuery.genre?.id,
-            parent_platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-          },
-        })
-        .then((res) => res.data),
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+        },
+      }),
   });
 
 export default useGames;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,8 @@
 import platforms from "../data/platforms";
-import apiClient, { FetchDataResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
+
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
 export interface Platform {
   id: number;
@@ -11,10 +13,7 @@ export interface Platform {
 const usePlatform = () =>
   useQuery({
     queryKey: ["platforms"],
-    queryFn: () =>
-      apiClient
-        .get<FetchDataResponse<Platform>>("/platforms/lists/parents")
-        .then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
     initialData: { count: platforms.length, results: platforms },
   });
