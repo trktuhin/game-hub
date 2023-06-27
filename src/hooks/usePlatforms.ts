628f2e2d0cfae0fb8f4dfd185b3927eb1824e152
trktuhin <robin35-908@diu.edu.bd>
2023-06-27 09:40:38 +0600

    Part 2 v38

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
-import { useQuery } from "@tanstack/react-query";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
@@ -14,7 +15,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
     initialData: platforms,
   });
 
